e0f6ca2d420c636975ee77fe747c4ccdd34d5d88
yagizdo <dokumaciyagiz@gmail.com>
2026-04-19 20:36:10 +0300

    refactor: remove json_serializable dependency and manual serialization methods from Flag model

--- lib/src/core/models/flag_model.dart
+++ lib/src/core/models/flag_model.dart
@@ -1,20 +1,12 @@
 import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
 
 import '../exceptions.dart';
 
-part 'flag_model.g.dart';
-
-@JsonSerializable()
 class Flag extends Equatable {
   final String key;
   final dynamic value;
 
   const Flag({required this.key, required this.value});
-
-  factory Flag.fromJson(Map<String, dynamic> json) => _$FlagFromJson(json);
-
-  Map<String, dynamic> toJson() => _$FlagToJson(this);
 
   @override
   List<Object?> get props => [key, value];

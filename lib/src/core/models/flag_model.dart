7eda2af29fd1749c234afb20ffeb79fd8f9b2744
yagizdo <dokumaciyagiz@gmail.com>
2026-04-19 15:34:33 +0300

    feat(flag): add typed value getters

--- lib/src/core/models/flag_model.dart
+++ lib/src/core/models/flag_model.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+import '../exceptions.dart';
+
 part 'flag_model.g.dart';
 
 @JsonSerializable()
@@ -16,4 +18,44 @@ class Flag extends Equatable {
 
   @override
   List<Object?> get props => [key, value];
+
+  bool getBool() {
+    final v = value;
+    if (v is bool) {
+      return v;
+    }
+    throw TypeMismatchException(flagKey: key, expectedType: bool, actualType: v.runtimeType);
+  }
+
+  String getString() {
+    final v = value;
+    if (v is String) {
+      return v;
+    }
+    throw TypeMismatchException(flagKey: key, expectedType: String, actualType: v.runtimeType);
+  }
+
+  int getInt() {
+    final v = value;
+    if (v is int) {
+      return v;
+    }
+    throw TypeMismatchException(flagKey: key, expectedType: int, actualType: v.runtimeType);
+  }
+
+  double getDouble() {
+    final v = value;
+    if (v is num) {
+      return v.toDouble();
+    }
+    throw TypeMismatchException(flagKey: key, expectedType: double, actualType: v.runtimeType);
+  }
+
+  Map<String, dynamic> getJson() {
+    final v = value;
+    if (v is Map<String, dynamic>) {
+      return v;
+    }
+    throw TypeMismatchException(flagKey: key, expectedType: Map<String, dynamic>, actualType: v.runtimeType);
+  }
 }
